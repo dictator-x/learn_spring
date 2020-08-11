7b6687df11029ac421dbe65498acd92d2891480e
dictator-x <dictator.xwu@gmail.com>
2020-08-10 21:33:14 -0400

    format

---
+++ spring-cloud-payment8002/src/main/java/com/myspring/cloud/eureka/payment/dao/PaymentDao.java
@@ -0,0 +1,7 @@
+package com.myspring.cloud.eureka.payment.dao;
+
+import com.myspring.cloud.commons.domain.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentDao extends JpaRepository<Payment, Long> {
+}
