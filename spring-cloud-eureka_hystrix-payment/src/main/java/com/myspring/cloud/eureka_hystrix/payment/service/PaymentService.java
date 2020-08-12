f1c2019d7ea01505cd601ff5072a10b621274ff9
dictator-x <dictator.xwu@gmail.com>
2020-08-12 14:03:50 -0400

    format

--- spring-cloud-eureka_hystrix-payment/src/main/java/com/myspring/cloud/eureka_hystrix/payment/service/PaymentService.java
+++ spring-cloud-eureka_hystrix-payment/src/main/java/com/myspring/cloud/eureka_hystrix/payment/service/PaymentService.java
@@ -2,7 +2,9 @@ package com.myspring.cloud.eureka_hystrix.payment.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.cloud.commons.util.IdUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.concurrent.TimeUnit;
 
@@ -29,6 +31,24 @@ public class PaymentService {
     public String paymentInfo_TimeOutHandler(Integer id) {
         return "Threadpool: " + Thread.currentThread().getName() + " paymentInfo_TimeOutHandler System busy, id: " + id + "\t" + "O(n-n)O";
 
+    }
+
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback", commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60")
+    })
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
+        if ( id < 0 ) {
+            throw new RuntimeException("id should not be negative");
+        }
+        String serialNumber = IdUtils.getUnresolvedServiceId();
+        return Thread.currentThread().getName() + "\t" + "call success, serial: " + serialNumber;
+    }
+
+    public String paymentCircuitBreaker_fallback(@PathVariable("id") Integer id) {
+        return "paymentCircuitBreaker_fallback - id: " + id;
     }
 
 }
